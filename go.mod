abd18a106d56e78ac936412d82a0507e9ea32028
jacexh <jace.xh@gmail.com>
2023-08-31 08:49:16 +0000

    add some tests for `NewHandler`

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.21
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0
-	github.com/imdario/mergo v0.3.15
-	github.com/pelletier/go-toml/v2 v2.0.7
+	github.com/imdario/mergo v0.3.16
+	github.com/pelletier/go-toml/v2 v2.1.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1
 )
--- sloghelper/handler_test.go
+++ sloghelper/handler_test.go
@@ -4,24 +4,31 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"sync/atomic"
 	"testing"
 
 	"github.com/go-jimu/components/sloghelper"
 )
 
 func TestNewHandler(t *testing.T) {
+	var called int64
 	ctx := context.WithValue(context.Background(), "foo", "bar")
 	hdl := slog.NewJSONHandler(os.Stdout, nil)
 	ch := sloghelper.NewHandler(
 		hdl,
 		sloghelper.WithDisableStackTrace(true),
 		sloghelper.WithHandleFunc(func(ctx context.Context, r *slog.Record) {
-			r.Add(slog.String("value", ctx.Value("foo").(string)))
+			r.AddAttrs(slog.Int64("called", atomic.AddInt64(&called, 1)))
 		}))
 	logger := slog.New(ch)
 	logger.ErrorContext(ctx, "world peace")
+	logger.InfoContext(ctx, "world peace again")
 
 	ch2 := sloghelper.NewHandler(ch)
 	logger2 := slog.New(ch2)
-	logger2.ErrorContext(ctx, "hello world")
+	logger2.WarnContext(ctx, "hello world")
+
+	if atomic.LoadInt64(&called) != 3 {
+		t.FailNow()
+	}
 }
