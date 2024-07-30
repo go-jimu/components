fb284de969142283333b2085747e1fe78962d042
jacexh <jace.xh@gmail.com>
2024-07-30 05:28:50 +0000

    遗漏了一些文件

---
+++ encoding/proto/proto.go
@@ -0,0 +1,50 @@
+// Package proto defines the protobuf codec. Importing this package will
+// register the codec.
+package proto
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/go-jimu/components/encoding"
+	"google.golang.org/protobuf/proto"
+)
+
+// Name is the name registered for the proto compressor.
+const Name = "proto"
+
+func init() {
+	encoding.RegisterCodec(codec{})
+}
+
+// codec is a codec implementation with protobuf. It is the default codec for Transport.
+type codec struct{}
+
+func (codec) Marshal(v interface{}) ([]byte, error) {
+	return proto.Marshal(v.(proto.Message))
+}
+
+func (codec) Unmarshal(data []byte, v interface{}) error {
+	pm, err := getProtoMessage(v)
+	if err != nil {
+		return err
+	}
+	return proto.Unmarshal(data, pm)
+}
+
+func (codec) Name() string {
+	return Name
+}
+
+func getProtoMessage(v interface{}) (proto.Message, error) {
+	if msg, ok := v.(proto.Message); ok {
+		return msg, nil
+	}
+	val := reflect.ValueOf(v)
+	if val.Kind() != reflect.Ptr {
+		return nil, errors.New("not proto message")
+	}
+
+	val = val.Elem()
+	return getProtoMessage(val.Interface())
+}
