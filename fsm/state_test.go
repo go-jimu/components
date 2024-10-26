a7ce7c98af49fb40a9bd31149497ef7ebda52b80
jacexh <jace.xh@gmail.com>
2024-10-26 16:30:34 +0000

    optimze the design of fsm

--- fsm/state_test.go
+++ fsm/state_test.go
@@ -11,11 +11,11 @@ import (
 
 // ShoppingCart is StateContext implementation.
 type ShoppingCart struct {
-	ID      string
-	State   ShoppingCartState
-	Machine fsm.StateMachine
-	Items   [10]string
-	Version int
+	ID               string
+	StateMachineName string
+	State            ShoppingCartState
+	Items            [10]string
+	Version          int
 }
 
 type (
@@ -59,7 +59,8 @@ const (
 func NewShoppingCart() *ShoppingCart {
 	state := NewEmptyState()
 	cart := &ShoppingCart{
-		Items: [10]string{},
+		Items:            [10]string{},
+		StateMachineName: "shopping_cart",
 	}
 	cart.TransitionTo(state)
 	return cart
@@ -74,33 +75,29 @@ func (sc *ShoppingCart) TransitionTo(state fsm.State) {
 	sc.State = state.(ShoppingCartState)
 }
 
-func (sc *ShoppingCart) SetStateMachine(machine fsm.StateMachine) {
-	sc.Machine = machine
-}
-
 func (sc *ShoppingCart) AddItem(_ ...string) error {
-	if !sc.Machine.HasTransition(sc.State.Label(), ActionAdd) {
+	sm := fsm.MustGetStateMachine(sc.StateMachineName)
+	if !sm.HasTransition(sc.State.Label(), ActionAdd) {
 		return fsm.NewTransitionError(sc.State.Label(), ActionAdd)
 	}
 	if err := sc.State.AddItem(); err != nil {
 		return err
 	}
 
-	next, _ := sc.Machine.TransitionToNext(sc.State.Label(), ActionAdd)
-	sc.TransitionTo(next)
+	_ = sm.TransitionToNext(sc, ActionAdd)
 	return nil
 }
 
 func (sc *ShoppingCart) Remove(_ ...string) error {
-	if !sc.Machine.HasTransition(sc.State.Label(), ActionRemove) {
+	sm := fsm.MustGetStateMachine(sc.StateMachineName)
+	if !sm.HasTransition(sc.State.Label(), ActionRemove) {
 		return fsm.NewTransitionError(sc.State.Label(), ActionRemove)
 	}
 	if err := sc.State.Remove(); err != nil {
 		return err
 	}
 
-	next, _ := sc.Machine.TransitionToNext(sc.State.Label(), ActionRemove)
-	sc.TransitionTo(next)
+	_ = sm.TransitionToNext(sc, ActionRemove)
 	return nil
 }
 
@@ -147,6 +144,7 @@ func (state *EmptyState) AddItem() error {
 
 func TestStateMachine(t *testing.T) {
 	sm := fsm.NewStateMachine("shopping_cart")
+	fsm.RegisterStateMachine(sm)
 	sm.RegisterStateBuilder(StateEmpty, NewEmptyState)
 	sm.RegisterStateBuilder(StateCheckedOut, func() fsm.State {
 		return fsm.NewSimpleState(StateCheckedOut)
@@ -169,6 +167,7 @@ func TestStateMachine(t *testing.T) {
 
 func TestStateContext(t *testing.T) {
 	sm := fsm.NewStateMachine("shopping_cart")
+	fsm.RegisterStateMachine(sm)
 	sm.RegisterStateBuilder(StateEmpty, NewEmptyState)
 	sm.RegisterStateBuilder(StateItemAdded, NewItemAddedState)
 
@@ -176,7 +175,6 @@ func TestStateContext(t *testing.T) {
 	assert.NoError(t, err)
 
 	sc := NewShoppingCart()
-	sc.SetStateMachine(sm)
 	assert.Equal(t, StateEmpty, sc.CurrentState().Label())
 	err = sc.AddItem()
 	assert.NoError(t, err)
